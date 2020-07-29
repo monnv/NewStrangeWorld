a14177ee4effeb135426bd8cf2a86c02871609ab
emma <emmakidwell@yahoo.com>
2020-07-29 15:15:13 -0400

    Added trigger for cat in StartLevel, got rid of 2 riddle options

---
+++ Assets/Scripts/OnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.tag == "Player")
+        {
+            SceneManager.LoadScene("Level01");
+        }
+    }
+}
